7b070fb53eb0802e0c4297ee78e28ce1183176c9
Walter Schulze <awalterschulze@gmail.com>
2015-09-29 15:22:57 +0200

    more complex example

---
+++ example.js
@@ -0,0 +1,41 @@
+var jsonDefault = JSON.stringify({
+	"WhatsUp": "E",
+	"Survived": 100000000,
+	"History": [
+		"Giant Lizards",
+		"Meteor",
+		"Lizards Dead",
+		"Katydids Alive"
+	],
+	"FeatureRequests": [
+		{
+			"Name": "Dart",
+			"Properties": ["Poison", "Projectile"],
+			"Location": "Tail"
+		},
+		{
+			"Name": "Fire Breath",
+			"Properties": ["Fire", "Vapor"],
+			"Location": "Mouth"
+		}
+	],
+	"DragonsExist": false,
+	"MonkeysSmart": true,
+	"Family": {
+		"Kingdom": "Animalia",
+		"Phylum": "Arthropoda",
+		"Class": "Insecta",
+		"Order": {
+			"Superorder": {
+				"Subclass":	"Pterygota",
+				"Infraclass": "Polyneoptera",
+				"Superorder": "Orthopterida"
+			},
+			"Order": "Orthoptera"
+		},
+		"Suborder":	"Ensifera",
+		"Infraorder": "Tettigoniidea",
+		"Superfamily": "Tettigonioidea",
+		"Family": "Tettigoniidae"
+	}
+}, "", 2);
